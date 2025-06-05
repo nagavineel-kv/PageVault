ee66331482d6ad788bf351dcdb57f04cb47044ca
Naga Vineel <nagavineel@Mac-285.lan>
2025-06-04 20:04:06 -0500

    Created Root files (auth)/_layout, index, signup & developed login(index) UI

---
+++ frontend/app/(auth)/index.jsx
@@ -0,0 +1,91 @@
+import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
+import styles from "../../assets/styles/login.styles";
+import { Link } from "expo-router";
+import React, { useState } from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from '../../constants/colors';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = () => {}
+  return (
+    <View style={styles.container}>
+      {/* ILLUSTRATION */}
+      <View style={styles.topIllustration}>
+        <Image source={require("../../assets/images/i.png")}
+               style={styles.illustrationImage}
+               resizeMode="contain"
+        />
+      </View>
+      <View style={styles.card}>
+        <View style={styles.formContainer}>
+          {/* Email */}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons 
+                name='mail-outline'
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+              />
+              <TextInput 
+                style={styles.input}
+                placeholder='Enter Your Email'
+                placeholderTextColor={Colors.placeholderTextColor}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType='email-address'
+                autoCapitalize='none'
+              />
+            </View>
+          </View>
+          {/* Password */}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Password</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons 
+                name='lock-closed-outline'
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+              />
+              <TextInput 
+                style={styles.input}
+                placeholder='Enter Your Password'
+                placeholderTextColor={Colors.placeholderTextColor}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={!showPassword}
+              />
+              <TouchableOpacity onPress={()=> setShowPassword(!showPassword)} style={styles.eyeIcon}>
+                <Ionicons 
+                  name={showPassword ? "eye-outline":"eye-off-outline"}
+                  size={20}
+                  color={COLORS.primary}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleLogin} disabled={isLoading}>
+            {isLoading ? (<ActivityIndicator color="#fff" />) : (<Text style={styles.buttonText}>Login</Text>)}
+          </TouchableOpacity>
+          {/** Footer */}
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>Don't have an account?</Text>
+            <Link href='/signup' asChild>
+              <TouchableOpacity>
+                <Text style={styles.link}>Sign Up</Text>
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}

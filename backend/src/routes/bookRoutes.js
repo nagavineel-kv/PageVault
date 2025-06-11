ee698f111308ad445cf97d507dcd3f27edd9d815
Naga Vineel <nagavineel@Mac.lan>
2025-06-10 20:45:01 -0500

    Modiefied bookRoute.js file

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -19,7 +19,6 @@ router.post("/", protectRoute, async (req, res) =>{
             caption,
             rating,
             image: imageUrl,
-            user: req.user._id, //added
         })
         await newBook.save();
         res.status(201).json(newBook);
@@ -35,7 +34,7 @@ router.get("/", protectRoute, async (req, res) => {
         const limit = req.query.limit || 5;
         const skip = (page - 1) * limit;
         const books = await Book.find().sort({createdAt: -1}).skip(skip).limit(limit).populate("user", "username profileImage");
-        const totalBooks = await Book.countDocuments(); //added
+        const totalBooks = await Book.countDocuments;
         res.send({
             books,
             currentPage: page,

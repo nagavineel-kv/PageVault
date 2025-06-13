d7a1cd99d43822fcac020e088f8bb57b28b2e203
Naga Vineel <nagavineel@Mac.lan>
2025-06-13 15:57:13 -0500

    Modiefied middleware.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,10 @@ job.start();
 app.use(express.json());
 app.use(cors());
 
+// ⬇️ Increase payload size limit here
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 
